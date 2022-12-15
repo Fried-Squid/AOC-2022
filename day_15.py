d17be7eec2f8f66ee24eaeed3d4d226808d4ce68
Ace <ctrlaltmarshmallow@gmail.com>
2022-12-15 22:01:21 +0000

    i actually despise this day.

--- day_15.py
+++ day_15.py
@@ -1,5 +1,5 @@
-from pipe import select, traverse
-import numpy as np
+from pipe import select, chain, where
+from itertools import combinations
 #pylint: disable=no-value-for-parameter
 
 def manhattan_dist(v1, v2):
@@ -8,6 +8,7 @@ def manhattan_dist(v1, v2):
     return abs(x1-x2) + abs(y1-y2) #utterly buckwild
 
 def solve_p1(data):
+    return "solved"
     data = list(data | select(lambda a: a.replace("Sensor at ", "").replace(" closest beacon is at ","").replace("y=","").replace("x=","")))
     data = list(data | select(lambda a: a.split(":")))
     data = list(data | select(lambda a: list(a | select(lambda b: b.split(",")))))
@@ -43,25 +44,12 @@ def solve_p1(data):
 
     return z
 
-
-def generate_closed_set(center, radius):
-    pointset = [center[:]]
-    cx, cy = center
-
-    for t in range(1, radius+1):
-        #x add, y minus
-        for i in range(1, radius+1):
-            pointset.append([cx+i,cy-t])
-        for i in range(1, radius+1):
-            pointset.append([cx+i,cy+t])
-        for i in range(1, radius+1):
-            pointset.append([cx-i,cy-t])
-        for i in range(1, radius+1):
-            pointset.append([cx-i,cy+t])
-
-    return pointset
-
-
+def check_point(data, point):
+    z=0
+    for beacon,dist in data:
+        if manhattan_dist(beacon,point) <= dist:
+            return False
+    return True
 
 def solve_p2(data):
     print("start p2")
@@ -69,14 +57,28 @@ def solve_p2(data):
     data = list(data | select(lambda a: a.split(":")))
     data = list(data | select(lambda a: list(a | select(lambda b: b.split(",")))))
     data = list(data | select(lambda a: list(a | select(lambda b: list(b | select(int))))))
-
     data = list(data | select(lambda a:[a[0], manhattan_dist(a[0], a[1])]))
-    beacons, distances = list(zip(*data))
-    basis = map(generate_closed_set, beacons, distances)
-    basis = list(set(list(basis | traverse)))
-    
-    points = [[[x,y] for y in range(0, 4000000)] for y in range(0, 4000000)]
 
-    return list(filter(lambda x:x not in basis, points))
+    upper = 4000000
+    to_check = []
+    for beacon, radius in data:
+        radius += 1
+        bx, by = beacon
 
+        for i in range(0, radius):
+            if 0 <= bx + radius - i <= upper and 0 <= by - i <= upper:
+                to_check.append((bx + radius-i, by-i)) ## +bx - -
+            if 0 <= bx - radius - i <= upper and 0 <= by + i <= upper:
+                to_check.append((bx - radius+i, by+i)) ## -bx + +
+            if 0 <= bx+i <= upper and 0 <= by + radius-i <= upper:
+                to_check.append((bx+i, by + radius-i)) ## +by + -
+            if 0 <= bx-i <= upper and 0 <= by - radius+i <= upper:
+                to_check.append((bx-i, by - radius+i)) ## -by + -
+
+    print(len(to_check))
+    for each in to_check:
+        x,y=each
+        if check_point(data, each) and 0<=x<=upper and 0<=y<=upper:
+            print(each)
+            print(x*upper + y)
 
