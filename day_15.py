4e0a77065b53caf78e2a94338ce24824379429a6
Ace <50378182+Fried-Squid@users.noreply.github.com>
2022-12-15 11:06:03 +0000

    day15 being annoying

--- day_15.py
+++ day_15.py
@@ -13,12 +13,27 @@ def solve_p1(data):
     data = list(data | select(lambda a: list(a | select(lambda b: list(b | select(int))))))
     # data = [[[Sx,Sy],[Bx,By]]]
 
-    # step 1: find the maximums
+    # step 1: find the maximums / mins
     max_x = max(list(list(data | select(lambda a: list(a | select(lambda b:b[0]))) | traverse)))
     max_y = max(list(list(data | select(lambda a: list(a | select(lambda b:b[1]))) | traverse)))
+    min_x = min(list(list(data | select(lambda a: list(a | select(lambda b:b[0]))) | traverse)))
+    min_y = min(list(list(data | select(lambda a: list(a | select(lambda b:b[1]))) | traverse)))
+    print(max_x, min_x, max_y, min_y)
+    data = list(data | select(lambda a: list(a | select(lambda b: [b[0]-min_x, b[1]-min_y]))))
 
     # step 2: construct the grid
-    grid = np.zeros((max_x, max_y)).tolist()
+    #grid = np.zeros((max_y-min_y, max_x-min_x), dtype=bool).tolist()
     
+    target_row = 10 #hardcoded target row
+
+    z=0 #think about the shape of the rectilinear distances maybe? irdk why this isnt working though it works fine on the example data
+    for i in range(max_x-min_x+1):
+        point = [i, target_row]
+        for sensor, beacon in data:
+            dist = manhattan_dist(sensor, beacon)
+            if manhattan_dist(point, sensor) <= dist or point == sensor or point == beacon:
+                z+=1
+                break
+    return z-1 #why
 def solve_p2(data):
     return None
